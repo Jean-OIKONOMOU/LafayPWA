3378c2af50e68180355b9367f6480b31dfec39ef
Jean <Jean-OIKONOMOU>
2024-03-02 13:40:56 +0100

    added workout images and sets

--- js/workout.js
+++ js/workout.js
@@ -4,79 +4,52 @@ var poses;
 
 //*********************************//
 const levelOnePoses = [
-    { name: "1", image: "img/stretching/1.png" },
-    { name: "2", image: "img/stretching/2.png" },
-    { name: "3", image: "img/stretching/3.png" },
-    { name: "4", image: "img/stretching/4.png" },
-    { name: "5", image: "img/stretching/5.png" },
-    { name: "6", image: "img/stretching/6.png" },
-    { name: "7", image: "img/stretching/7.png" },
-    { name: "8", image: "img/stretching/8.png" },
-    { name: "9", image: "img/stretching/9.png" },
-    { name: "10", image: "img/stretching/10.png" },
-    { name: "11", image: "img/stretching/11.png" },
-    { name: "12", image: "img/stretching/12.png" },
-    { name: "13", image: "img/stretching/13.png" },
-    { name: "14", image: "img/stretching/14.png" },
-    { name: "15", image: "img/stretching/15.png" },
-    { name: "16", image: "img/stretching/16.png" },
-    { name: "17", image: "img/stretching/17.png" },
-    { name: "18", image: "img/stretching/18.png" },
-    { name: "19", image: "img/stretching/19.png" },
-    { name: "20", image: "img/stretching/20.png" },
-    { name: "21", image: "img/stretching/21.png" },
-    { name: "22", image: "img/stretching/22.png" },
-    { name: "23", image: "img/stretching/23.png" },
-    { name: "24", image: "img/stretching/24.png" },
-    { name: "25", image: "img/stretching/25.png" },
-    { name: "26", image: "img/stretching/26.png" },
-    { name: "27", image: "img/stretching/27.png" },
-    { name: "28", image: "img/stretching/28.png" },
-    { name: "29", image: "img/stretching/29.png" },
-    { name: "30", image: "img/stretching/30.png" },
-    { name: "31", image: "img/stretching/31.png" },
-    { name: "32", image: "img/stretching/32.png" },
-    { name: "33", image: "img/stretching/33.png" },
-    { name: "34", image: "img/stretching/34.png" },
-    { name: "35", image: "img/stretching/35.png" }
+    { name: "1", image: "img/stretching/1.png", sets: 1 },
+    { name: "2", image: "img/stretching/2.png", sets: 1 },
+    { name: "3", image: "img/stretching/3.png", sets: 1 },
+    { name: "4", image: "img/stretching/4.png", sets: 1 },
+    { name: "5", image: "img/stretching/5.png", sets: 1 },
+    { name: "6", image: "img/stretching/6.png", sets: 1 },
+    { name: "7", image: "img/stretching/7.png", sets: 1 },
+    { name: "8", image: "img/stretching/8.png", sets: 1 },
+    { name: "9", image: "img/stretching/9.png", sets: 1 },
+    { name: "10", image: "img/stretching/10.png", sets: 1 },
+    { name: "11", image: "img/stretching/11.png", sets: 1 },
+    { name: "12", image: "img/stretching/12.png", sets: 1 },
+    { name: "13", image: "img/stretching/13.png", sets: 1 },
+    { name: "14", image: "img/stretching/14.png", sets: 1 },
+    { name: "15", image: "img/stretching/15.png", sets: 1 },
+    { name: "16", image: "img/stretching/16.png", sets: 1 },
+    { name: "17", image: "img/stretching/17.png", sets: 1 },
+    { name: "18", image: "img/stretching/18.png", sets: 1 },
+    { name: "19", image: "img/stretching/19.png", sets: 1 },
+    { name: "20", image: "img/stretching/20.png", sets: 1 },
+    { name: "21", image: "img/stretching/21.png", sets: 1 },
+    { name: "22", image: "img/stretching/22.png", sets: 1 },
+    { name: "23", image: "img/stretching/23.png", sets: 1 },
+    { name: "24", image: "img/stretching/24.png", sets: 1 },
+    { name: "25", image: "img/stretching/25.png", sets: 1 },
+    { name: "26", image: "img/stretching/26.png", sets: 1 },
+    { name: "27", image: "img/stretching/27.png", sets: 1 },
+    { name: "28", image: "img/stretching/28.png", sets: 1 },
+    { name: "29", image: "img/stretching/29.png", sets: 1 },
+    { name: "30", image: "img/stretching/30.png", sets: 1 },
+    { name: "31", image: "img/stretching/31.png", sets: 1 },
+    { name: "32", image: "img/stretching/32.png", sets: 1 },
+    { name: "33", image: "img/stretching/33.png", sets: 1 },
+    { name: "34", image: "img/stretching/34.png", sets: 1 },
+    { name: "35", image: "img/stretching/35.png", sets: 1 }
 ];
 
 const levelTwoPoses = [
-    { name: "1", image: "img/stretching/1.png" },
-    { name: "2", image: "img/stretching/2.png" },
-    { name: "3", image: "img/stretching/3.png" },
-    { name: "4", image: "img/stretching/4.png" },
-    { name: "5", image: "img/stretching/5.png" },
-    { name: "6", image: "img/stretching/6.png" },
-    { name: "7", image: "img/stretching/7.png" },
-    { name: "8", image: "img/stretching/8.png" },
-    { name: "9", image: "img/stretching/9.png" },
-    { name: "10", image: "img/stretching/10.png" },
-    { name: "11", image: "img/stretching/11.png" },
-    { name: "12", image: "img/stretching/12.png" },
-    { name: "13", image: "img/stretching/13.png" },
-    { name: "14", image: "img/stretching/14.png" },
-    { name: "15", image: "img/stretching/15.png" },
-    { name: "16", image: "img/stretching/16.png" },
-    { name: "17", image: "img/stretching/17.png" },
-    { name: "18", image: "img/stretching/18.png" },
-    { name: "19", image: "img/stretching/19.png" },
-    { name: "20", image: "img/stretching/20.png" },
-    { name: "21", image: "img/stretching/21.png" },
-    { name: "22", image: "img/stretching/22.png" },
-    { name: "23", image: "img/stretching/23.png" },
-    { name: "24", image: "img/stretching/24.png" },
-    { name: "25", image: "img/stretching/25.png" },
-    { name: "26", image: "img/stretching/26.png" },
-    { name: "27", image: "img/stretching/27.png" },
-    { name: "28", image: "img/stretching/28.png" },
-    { name: "29", image: "img/stretching/29.png" },
-    { name: "30", image: "img/stretching/30.png" },
-    { name: "31", image: "img/stretching/31.png" },
-    { name: "32", image: "img/stretching/32.png" },
-    { name: "33", image: "img/stretching/33.png" },
-    { name: "34", image: "img/stretching/34.png" },
-    { name: "35", image: "img/stretching/35.png" }
+    { name: "1", image: "img/workout/B.png" , sets: 3 },
+    { name: "2", image: "img/workout/A1.png", sets: 3 },
+    { name: "3", image: "img/workout/D.png" , sets: 1 },
+    { name: "4", image: "img/workout/C4.png", sets: 3 },
+    { name: "5", image: "img/workout/E.png" , sets: 3 },
+    { name: "6", image: "img/workout/F.png" , sets: 3 },
+    { name: "7", image: "img/workout/G.png" , sets: 3 },
+    { name: "8", image: "img/workout/K2.png", sets: 3 }
 ];
 
 
@@ -117,7 +90,7 @@ const levelOne = (images) => {
     showControls();
     
     let output = ""
-    output += `<div class="card"><img class="card--avatar" id="stretchingImage" src=${images[0].image} alt=${images[0].name}/></div>`;
+    output += `<div class="card"><img class="card--avatar" id="stretchingImage" src=${images[0].image} alt=${images[0].name}/><h1 style="margin-top:10px;font-size:2rem;">${images[0].sets} sets</h1></div>`;
     container.innerHTML = output;
 
     poses = levelOnePoses;
@@ -127,7 +100,7 @@ const levelTwo = (images) => {
     showControls();
 
     let output = ""
-    output += `<div class="card"><img class="card--avatar" id="stretchingImage" src=${images[0].image} alt=${images[0].name}/></div>`;
+    output += `<div class="card"><img class="card--avatar" id="stretchingImage" src=${images[0].image} alt=${images[0].name}/><h1 style="margin-top:10px;font-size:2rem;">${images[0].sets} sets</h1></div>`;
     container.innerHTML = output;
 
     poses = levelTwoPoses;
